661212073b5811f892bf07ecd32d139829e96d81
Jiwon Kwon <work@kwonjiwon.org>
2026-09-15 14:21:25 +0900

    Add todo message

--- packages/web/src/routes/workspace/create/[instance_id]/actors.tsx
+++ packages/web/src/routes/workspace/create/[instance_id]/actors.tsx
@@ -146,6 +146,7 @@ export default function CreateActorsPage(props: RouteSectionProps<RouteData>) {
           return;
         }
 
+        // TODO: Replace current codes to dictionary looking up
         const result = response.generateActors;
         switch (result.resultType) {
           case "CreateActorsSuccess": {
